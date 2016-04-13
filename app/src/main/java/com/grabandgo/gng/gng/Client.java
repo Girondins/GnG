84882f264413d3f64084f9bc354f9e2a424b545a
Girondins <stmsfsd@gmail.com>
2016-04-13 17:00:26 +0200

    Client Controller Change
    Contact with Client
    Controller Create Client

--- app/src/main/java/com/grabandgo/gng/gng/Client.java
+++ app/src/main/java/com/grabandgo/gng/gng/Client.java
@@ -1,8 +1,80 @@
 package com.grabandgo.gng.gng;
 
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 /**
  * Created by alexander on 2016-04-13.
  */
-public class Client {
+public class Client extends Service implements Runnable{
+
+    private Controller controller;
+    private Socket socket;
+    private ObjectOutputStream oos;
+    private ObjectInputStream ois;
+    private String ip;
+    private int port;
+    private final IBinder mbinder = new LocalBinder();
+    private Thread thread = new Thread(this);
+
+    public Client(String ip, int port) throws UnknownHostException, IOException {
+        this.ip = ip;
+        this.port = port;
+        thread.start();
+    }
+
+    @Override
+    public void run() {
+
+        try {
+            socket = new Socket(ip,port);
+            ois = new ObjectInputStream(new BufferedInputStream(socket.getInputStream()));
+            oos = new ObjectOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        new ClientStarter().start();
+
+    }
+
+    public class LocalBinder extends Binder {
+
+        public Client getClient(){
+            return Client.this;
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mbinder;
+    }
+
+    public void setClientController(Controller controller) {
+        this.controller = controller;
+
+    }
+
+
+    private class ClientStarter extends Thread {
+        public void run() {
+            Object object;
+            while (true) {
+
+
+            }
+        }
+    }
 
 }
--- app/src/main/java/com/grabandgo/gng/gng/Controller.java
+++ app/src/main/java/com/grabandgo/gng/gng/Controller.java
@@ -1,7 +1,20 @@
 package com.grabandgo.gng.gng;
 
+import java.io.IOException;
+
 /**
  * Created by alexander on 2016-04-13.
  */
 public class Controller {
+    private Client client;
+
+    public Controller(){
+        try {
+            client = new Client("192.168.0.1",9000);
+            client.setClientController(this);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
