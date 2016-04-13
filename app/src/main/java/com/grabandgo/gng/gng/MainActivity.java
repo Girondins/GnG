59dbf96f3fbb46f5415263f229e03d9b71868dfe
Nyandux <ragnar.einestam@hotmail.com>
2016-04-13 14:55:30 +0200

    Test v2

--- app/src/main/java/com/grabandgo/gng/gng/MainActivity.java
+++ app/src/main/java/com/grabandgo/gng/gng/MainActivity.java
@@ -8,19 +8,14 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
+/**
+ * Main activity, GNG Map and navigation.
+ */
 public class MainActivity extends Activity {
     private LocationManager locationManager;
     private LocationListener locationListener;
